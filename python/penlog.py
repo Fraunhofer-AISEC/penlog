6ee181f009cea3a472e6a02776fd055623139e16
Stefan Tatschner <stefan@rumpelsepp.org>
2020-06-02 15:27:31 +0200

    Add a python module level logger
    This is restricted in scope but useful… And we use it because of
    history. :D

--- python/penlog.py
+++ python/penlog.py
@@ -103,3 +103,72 @@ class Logger:
 
     def log_summary(self, data: str, tags: Optional[List[str]] = None) -> None:
         self._log_msg(data, MessageType.SUMMARY, MessagePrio.NOTICE, tags)
+
+
+# This is the module level default logging emulation.
+
+def _log(msg: Dict, depth: int) -> None:
+    msg["component"] = "root"
+    msg["host"] = socket.gethostname()
+    msg["timestamp"] = datetime.now().isoformat()
+    if os.environ.get("PENLOG_LINES"):
+        msg["line"] = _get_line_number(depth)
+    print(json.dumps(msg), file=sys.stderr, flush=True)
+
+
+def _log_msg(data: str, type_: MessageType = MessageType.MESSAGE,
+             prio: MessagePrio = MessagePrio.INFO,
+             tags: Optional[List[str]] = None) -> None:
+    msg = {
+        'type': type_,
+        'priority': prio,
+        'data': data,
+    }
+    if tags:
+        msg['tags'] = tags
+    _log(msg, 4)
+
+
+def log_msg(data: str, type_: MessageType = MessageType.MESSAGE,
+            prio: MessagePrio = MessagePrio.INFO,
+            tags: Optional[List[str]] = None) -> None:
+    msg = {
+        'type': type_,
+        'priority': prio,
+        'data': data,
+    }
+    if tags:
+        msg['tags'] = tags
+    _log(msg, 3)
+
+
+def log_preamble(data: str) -> None:
+    _log_msg(data, MessageType.PREAMBLE, MessagePrio.NOTICE)
+
+
+def log_read(data: str, tags: Optional[List[str]] = None) -> None:
+    _log_msg(data, MessageType.READ, MessagePrio.DEBUG, tags)
+
+
+def log_write(data: str, tags: Optional[List[str]] = None) -> None:
+    _log_msg(data, MessageType.WRITE, MessagePrio.DEBUG, tags)
+
+
+def log_debug(data: str, tags: Optional[List[str]] = None) -> None:
+    _log_msg(data, MessageType.MESSAGE, MessagePrio.DEBUG, tags)
+
+
+def log_info(data: str, tags: Optional[List[str]] = None) -> None:
+    _log_msg(data, MessageType.MESSAGE, MessagePrio.INFO, tags)
+
+
+def log_warning(data: str, tags: Optional[List[str]] = None) -> None:
+    _log_msg(data, MessageType.MESSAGE, MessagePrio.WARNING, tags)
+
+
+def log_error(data: str, tags: Optional[List[str]] = None) -> None:
+    _log_msg(data, MessageType.MESSAGE, MessagePrio.ERROR, tags)
+
+
+def log_summary(data: str, tags: Optional[List[str]] = None) -> None:
+    _log_msg(data, MessageType.SUMMARY, MessagePrio.NOTICE, tags)
