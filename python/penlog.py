4d6308affa7c50ad6d3e3aa42ae90f4d45ee36ae
Stefan Tatschner <stefan@rumpelsepp.org>
2020-06-25 16:43:05 +0200

    Add formatting magic in python as well

--- python/penlog.py
+++ python/penlog.py
@@ -35,29 +35,151 @@ class MessagePrio(IntEnum):
     DEBUG = 7
 
 
+class OutputType(Enum):
+    JSON = "json"
+    JSON_PRETTY = "json-pretty"
+    HR = "hr"
+    HR_TINY = "hr-tiny"
+
+
+class Colors(Enum):
+    NOP = ""
+    RESET = "\033[0m"
+    BOLD = "\033[1m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    PURPLE = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    GRAY = "\033[0;38;5;245m"
+
+
+def colorize(color: Colors, s: str) -> str:
+    if color == Colors.NOP:
+        return s
+    return f"{color.value}{s}{Colors.RESET.value}"
+
+
 def _get_line_number(depth: int) -> str:
     stack = inspect.stack()
     frame = stack[depth]
     return f'{frame.filename}:{frame.lineno}'
 
 
+def str2bool(s: str) -> bool:
+    return s.lower() in ['true', '1', 't', 'y']
+
+
+class HRFormatter:
+
+    def __init__(self, show_colors: bool, show_ids: bool,
+                 show_lines: bool, show_stacktraces: bool,
+                 show_tags: bool, tiny: bool):
+        self.show_colors = show_colors
+        self.show_ids = show_ids
+        self.show_lines = show_lines
+        self.show_stacktraces = show_stacktraces
+        self.show_tags = show_tags
+        self.tiny = tiny
+
+    def format(self, msg: Dict) -> str:
+        out = ""
+        ts = datetime.fromisoformat(msg["timestamp"])
+        ts_formatted = ts.strftime("%b %m %H:%M:%S.%f")[:-3]
+        component = msg["component"]
+        msgtype = msg["type"]
+        data = msg["data"]
+        if self.show_colors and "priority" in msg:
+            prio = MessagePrio(msg["priority"])
+            if prio == MessagePrio.EMERGENCY or \
+                    prio == MessagePrio.ALERT or \
+                    prio == MessagePrio.CRITICAL or \
+                    prio == MessagePrio.ERROR:
+                data = colorize(Colors.BOLD, colorize(Colors.RED, data))
+            elif prio == MessagePrio.WARNING:
+                data = colorize(Colors.BOLD, colorize(Colors.YELLOW, data))
+            elif prio == MessagePrio.NOTICE:
+                data = colorize(Colors.BOLD, data)
+            elif prio == MessagePrio.INFO:
+                pass
+            elif prio == MessagePrio.DEBUG:
+                data = colorize(Colors.GRAY, data)
+        if self.tiny:
+            out = f"{ts_formatted}: {data}"
+        else:
+            out = f"{ts_formatted} {{{component: <8}}} [{msgtype: <8}]: {data}"
+        if self.show_ids and "id" in msg:
+            out += "\n"
+            if self.show_colors:
+                out += f" => id  : {colorize(Colors.YELLOW, msg['id'])}"
+            else:
+                out += f" => id  : {msg['id']}"
+        if self.show_lines and "line" in msg:
+            out += "\n"
+            if self.show_colors:
+                out += f" => line: {colorize(Colors.BLUE, msg['line'])}"
+            else:
+                out += f" => line: {msg['line']}"
+        if self.show_tags and "tags" in msg:
+            out += "\n"
+            out += f" => tags: {' '.join(msg['tags'])}"
+        if self.show_stacktraces and "stacktrace" in msg:
+            out += "\n"
+            out += " => stacktrace:\n"
+            for line in msg['stacktrace']:
+                if self.show_colors:
+                    out += colorize(Colors.GRAY, f" | {line}")
+                else:
+                    out += f" | {line}"
+        return out
+
+
 class Logger:
-    def __init__(self, component: str = "root", timefmt: str = '%c',
-                 flush: bool = False, file_: TextIO = sys.stderr):
+    def __init__(self, component: str = "root", flush: bool = False,
+                 file_: TextIO = sys.stderr,
+                 output_type: Optional[OutputType] = None,
+                 show_colors: bool = False):
         self.host = socket.gethostname()
         self.component = component
         self.flush = flush
         self.file = file_
+        if output_type:
+            self.output_type = output_type
+        else:
+            output_type_raw = os.environ.get("PENLOG_OUTPUT")
+            if output_type_raw is None:
+                self.output_type = OutputType.HR_TINY
+            else:
+                self.output_type = OutputType(output_type_raw)
+        self.lines = str2bool(os.environ.get("PENLOG_CAPTURE_LINES", ""))
+        self.stacktraces = str2bool(os.environ.get("PENLOG_CAPTURE_STACKTRACES", ""))
+        is_tiny = True if self.output_type == OutputType.HR_TINY else False
+        self.hr_formatter = HRFormatter(show_colors, False, self.lines,
+                                        self.stacktraces, False, is_tiny)
 
     def _log(self, msg: Dict, depth: int) -> None:
         msg["component"] = self.component
         msg["host"] = self.host
         msg["timestamp"] = datetime.now().isoformat()
-        if os.environ.get("PENLOG_CAPTURE_LINES"):
+        if self.lines:
             msg["line"] = _get_line_number(depth)
-        if os.environ.get("PENLOG_CAPTURE_STACKTRACES"):
+        if self.stacktraces:
             msg["stacktrace"] = ''.join(traceback.format_stack())
-        print(json.dumps(msg), file=self.file, flush=self.flush)
+        if self.output_type == OutputType.JSON:
+            print(json.dumps(msg), file=self.file, flush=self.flush)
+        elif self.output_type == OutputType.JSON_PRETTY:
+            print(json.dumps(msg, indent=2), file=self.file, flush=self.flush)
+        elif self.output_type == OutputType.HR:
+            out = self.hr_formatter.format(msg)
+            print(out, file=self.file, flush=self.flush)
+        elif self.output_type == OutputType.HR_TINY:
+            out = self.hr_formatter.format(msg)
+            print(out, file=self.file, flush=self.flush)
+        else:
+            print("invalid penlog output", file=sys.stderr)
+            sys.exit(1)
 
     def _log_msg(self, data: str, type_: MessageType = MessageType.MESSAGE,
                  prio: MessagePrio = MessagePrio.INFO,
@@ -113,81 +235,3 @@ class Logger:
     def log_summary(self, data: str, tags: Optional[List[str]] = None) -> None:
         self._log_msg(data, MessageType.SUMMARY, MessagePrio.NOTICE, tags)
 
-
-# This is the module level default logging emulation.
-
-def _log(msg: Dict, depth: int) -> None:
-    msg["component"] = "root"
-    msg["host"] = socket.gethostname()
-    msg["timestamp"] = datetime.now().isoformat()
-    if os.environ.get("PENLOG_CAPTURE_LINES"):
-        msg["line"] = _get_line_number(depth)
-    if os.environ.get("PENLOG_CAPTURE_STACKTRACES"):
-        msg["stacktrace"] = ''.join(traceback.format_stack())
-    print(json.dumps(msg), file=sys.stderr, flush=True)
-
-
-def _log_msg(data: str, type_: MessageType = MessageType.MESSAGE,
-             prio: MessagePrio = MessagePrio.INFO,
-             tags: Optional[List[str]] = None) -> None:
-    msg = {
-        'type': type_,
-        'priority': prio,
-        'data': data,
-    }
-    if tags:
-        msg['tags'] = tags
-    _log(msg, 4)
-
-
-def log_msg(data: str, type_: MessageType = MessageType.MESSAGE,
-            prio: MessagePrio = MessagePrio.INFO,
-            tags: Optional[List[str]] = None) -> None:
-    msg = {
-        'type': type_,
-        'priority': prio,
-        'data': data,
-    }
-    if tags:
-        msg['tags'] = tags
-    _log(msg, 3)
-
-
-def log_preamble(data: str) -> None:
-    _log_msg(data, MessageType.PREAMBLE, MessagePrio.NOTICE)
-
-
-def log_read(data: str, tags: Optional[List[str]] = None) -> None:
-    _log_msg(data, MessageType.READ, MessagePrio.DEBUG, tags)
-
-
-def log_write(data: str, tags: Optional[List[str]] = None) -> None:
-    _log_msg(data, MessageType.WRITE, MessagePrio.DEBUG, tags)
-
-
-def log_debug(data: str, tags: Optional[List[str]] = None) -> None:
-    _log_msg(data, MessageType.MESSAGE, MessagePrio.DEBUG, tags)
-
-
-def log_info(data: str, tags: Optional[List[str]] = None) -> None:
-    _log_msg(data, MessageType.MESSAGE, MessagePrio.INFO, tags)
-
-
-def log_notice(data: str, tags: Optional[List[str]] = None) -> None:
-    _log_msg(data, MessageType.MESSAGE, MessagePrio.NOTICE, tags)
-
-
-def log_warning(data: str, tags: Optional[List[str]] = None) -> None:
-    _log_msg(data, MessageType.MESSAGE, MessagePrio.WARNING, tags)
-
-
-def log_error(data: str, tags: Optional[List[str]] = None) -> None:
-    _log_msg(data, MessageType.MESSAGE, MessagePrio.ERROR, tags)
-
-
-def log_critical(data: str, tags: Optional[List[str]] = None) -> None:
-    _log_msg(data, MessageType.MESSAGE, MessagePrio.CRITICAL, tags)
-
-
-def log_summary(data: str, tags: Optional[List[str]] = None) -> None:
-    _log_msg(data, MessageType.SUMMARY, MessagePrio.NOTICE, tags)
